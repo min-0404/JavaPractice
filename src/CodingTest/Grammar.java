a3abd85b5f1da0bea0a3b955f87a222b479da293
Minseok Kim <minsuck040484@gmail.com>
2023-01-04 18:16:30 +0900

    .

---
+++ src/CodingTest/Grammar.java
@@ -0,0 +1,20 @@
+package CodingTest;
+import java.util.*; // 유틸 라이브러리
+import java.io.*; // 입출력 라이브러리
+
+public class Grammar {
+
+    public static void main(String[] args) {
+
+        // 1. 변수 선언
+        String[] arr1 = new String[5];
+        int[] arr2 = {1, 2, 3};
+        int N = 3;
+        int[] arr3 = new int[N];
+
+        // 2. 배열 다루기
+        int x[] = {1,2,3};
+
+
+    }
+}
